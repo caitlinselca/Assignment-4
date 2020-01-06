a54903a35771a14c650b16d0d05a3334960c5b72
Khinshan Khan <khanshan501@gmail.com>
2020-01-06 14:42:33 -0500

    benchmark work

---
+++ script.js
@@ -0,0 +1,11 @@
+const right = () => {
+  document.getElementById("ans").innerHTML = "I'm right";
+};
+
+const noMe = () => {
+  document.getElementById("ans").innerHTML = "No, I'm right!";
+};
+
+const dontHover = () => {
+  alert("Hey, I told you not to hover over me!");
+};
