2ee2c5f4ed4e14fee1174618eb83cc28e0a544d5
Khinshan Khan <khanshan501@gmail.com>
2020-01-06 14:55:06 -0500

    benchmark #2, need to add css now

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ const noMe = () => {
 const dontHover = () => {
   alert("Hey, I told you not to hover over me!");
 };
+
+const validate = () => {
+  if (document.getElementById("psw").value != 12345678) {
+    alert("Password Incorrect!");
+  } else {
+    document.querySelector("h1").innerHTML = "Logged in";
+  }
+};
